cca0386bac4d76bbc963bf2bf3154fed15c5dd88
saeed <s>
2019-08-12 16:32:10 +0430

    PersianCurrencyPipe updated

--- src/core/admin/shared/pipes/persian-currency.pipe.ts
+++ src/core/admin/shared/pipes/persian-currency.pipe.ts
@@ -1,5 +1,5 @@
-import { Pipe, PipeTransform } from '@angular/core';
-import { CurrencyPipe } from '@angular/common';
+import { Pipe, PipeTransform } from "@angular/core";
+import { CurrencyPipe } from "@angular/common";
 
 @Pipe({
   name: "persianCurrency"
@@ -9,6 +9,9 @@ export class PersianCurrencyPipe implements PipeTransform {
   transform(value: string, currencyCode: any = "IRR"): string {
     if (currencyCode == "IRR") {
       let tmp = this.currencyPipe.transform(value, currencyCode);
+      if (tmp.localeCompare("0 IRR")) {
+        return "0";
+      }
       if (tmp) {
         return tmp.replace(currencyCode, "تومان");
       }
